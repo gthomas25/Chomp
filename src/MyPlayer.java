50eeac11966097b52619b7f7dbf258eb210ccc1f
GThomas25 <gavin_thomas25@milton.edu>
2024-06-03 09:04:01 -0400

    mmm

--- src/MyPlayer.java
+++ src/MyPlayer.java
@@ -4,6 +4,7 @@ public class MyPlayer {
     public Chip[][] gameBoard;
     public int[] columns;
 
+
     public MyPlayer() {
         columns = new int[10];
         System.out.println("hi");
@@ -18,21 +19,47 @@ for (int a=1;a<=3;a++){
             for (int b=0;b<=3;b++){
                 for (int c=0;c<=3;c++){
                     if (c<=b && b <= a){
+                        int C=c;
+                        int B=b;
+                        System.out.println("Board");
                         System.out.println(a+","+b+","+c);
                         System.out.println("resulting boards: ");
                         for (int z=c-1;z>=0;z--){
                             System.out.println("("+a+", "+b+", "+z+")");
+                            if (a==1 && b==0 && z==0){
+                                System.out.println("Death Board found");
+                            }
                         }
-                        for (int y=b-1;y>=b;y--){
-
-                            //System.out.println(+a+", "+y+", "+c +")");
+                        for (int y=b-1;y>=0;y--){
+                            if(C> y) {
+                                C = y;
+                            }
+                           System.out.println(+a+", "+y+", "+C +")");
+                            if (a==1 && y==0 && C==0){
+                                System.out.println("Death Board found");
+                            }
 
                         }
-                        for (int q=1;q<a;q++){
+                         C=c;
 
-                            System.out.println("("+q+", "+b+", "+c+")");
+                        for (int q=a-1;q>=0;q--){
+                            if(B> q) {
+                                B = q;
+                            }
+                            if(C> q) {
+                                C = q;
+                            }
+
+
+                            System.out.println("("+q+", "+B+", "+C+")");
+                            if (q==1 && B==0 && C==0){
+                                System.out.println("Death Board found");
+                            }
+
 
                         }
+
+
 
 
                     }
@@ -51,6 +78,8 @@ for (int a=1;a<=3;a++){
 
         row = 1;
         column = 1;
+
+
 
         /***
          * This code will run each time the "MyPlayer" button is pressed.
